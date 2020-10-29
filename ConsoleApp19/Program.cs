9e6096d7a5e835d7469448e2956ad9c32aa70b95
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-10-28 21:55:48 -0400

    added

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 123;
+            int reversed = 0;
+            int pop;
+
+            while(x !=0)
+            {
+                pop = x % 10;
+                x = x / 10;
+                reversed = (reversed * 10) + pop;
+            }
+            Console.WriteLine(reversed);
+            Console.ReadLine();
+
+        }
+    }
+}
